87f03feab55b6c09e385b41739da3cdafb44e5a8
Zid Daniel <danielzid09@gmail.com>
2026-03-05 13:32:10 -0500

    Add body movement trail

--- body.py
+++ body.py
@@ -1,6 +1,9 @@
 # body.py
 import pygame
 from math import sqrt
+from collections import deque
+
+TRAIL_LENGTH = 40
 
 class Body:
     def __init__(self, x, y, mass):
@@ -16,13 +19,30 @@ class Body:
         self.mass = mass
         self.radius = max(3, int(sqrt(mass)))
 
+        # trail history
+        self.trail = deque(maxlen=TRAIL_LENGTH)
+
     def update(self, dt):
+        # store previous position
+        self.trail.append((self.x, self.y))
+
         # integrate velocity
         self.x += self.vx * dt
         self.y += self.vy * dt
 
     def draw(self, surf):
-        # color by mass
+        # draw trail
+        pts = list(self.trail)
+        for i in range(1, len(pts)):
+            pygame.draw.line(
+                surf,
+                (180, 180, 180),
+                (int(pts[i-1][0]), int(pts[i-1][1])),
+                (int(pts[i][0]),   int(pts[i][1])),
+                1
+            )
+
+        # draw body
         color = self.body_color()
         pygame.draw.circle(surf, color, (int(self.x), int(self.y)), self.radius)
 
